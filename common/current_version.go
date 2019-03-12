7da4c8d2adf996dad97f5633bbc32e9beb548d61
Clinton Bush <clinton.bush@cgifederal.com>
2019-03-12 09:03:13 -0400

    Bumped minor version due to GoDog update

--- common/current_version.go
+++ common/current_version.go
@@ -3,6 +3,6 @@ package common
 // CurrentVersion is the current version of Kosher; the one, true version number in this code base.
 var CurrentVersion = &Version{
 	Major: 1,
-	Minor: 0,
+	Minor: 1,
 	Patch: 0,
 }
