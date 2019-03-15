1bbfab783b60be0f11e5d9427f567769daed2076
Clinton Bush <clinton.bush@cgifederal.com>
2019-03-15 15:07:45 -0400

    fixed i should see step

--- steps/steputils/steputils.go
+++ steps/steputils/steputils.go
@@ -100,8 +100,6 @@ func (s *StepUtils) ResolveSelector(name string) ([]*agouti.Selection, error) {
 			for i := 0; i < matchCnt; i++ {
 				if visible, _ := agoutiSel.At(i).Visible(); visible || !ignoreInvisible {
 					results = append(results, agoutiSel.At(i))
-				} else {
-					fmt.Printf("%t\n", visible)
 				}
 			}
 		}
