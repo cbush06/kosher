c26119d5c79cf5b8414f9b813f0673c06826347b
Clinton Bush <clinton.bush@cgifederal.com>
2019-04-29 16:40:51 -0400

    Bumped version

--- common/current_version.go
+++ common/current_version.go
@@ -3,6 +3,6 @@ package common
 // CurrentVersion is the current version of Kosher; the one, true version number in this code base.
 var CurrentVersion = &Version{
 	Major: 1,
-	Minor: 5,
+	Minor: 6,
 	Patch: 0,
 }
